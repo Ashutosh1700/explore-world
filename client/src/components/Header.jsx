e1c7b7ac0e36d99455dcd961daed5cf1f2f2eb18
Ashutosh1700 <94374532+Ashutosh1700@users.noreply.github.com>
2024-03-16 12:06:07 +0530

    create test api

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -10,8 +10,7 @@ const Header = () => {
         <Navbar className=' border-b-2'>
             <Link to="/" className=' self-center whitespace-nowrap text-sm sm:text-xl
              font-semibold dark:text-white'>
-                <span className='px-2 py-1 bg-gradient-to-r from-indigo-500
-                 via-purple-500 to-pink-500 rounded-lg text-white'>AkPatel's</span>
+                <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% ... rounded-lg text-white'>AkPatel's</span>
                 Blog
             </Link>
             <form>
